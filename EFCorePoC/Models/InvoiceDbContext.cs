08315034f3663e8c175c0337cb91041b20feddbe
RichNGit <richard.newbould@sage.com>
2019-04-11 11:12:53 +0100

    Adding Products

--- EFCorePoC/Models/InvoiceDbContext.cs
+++ EFCorePoC/Models/InvoiceDbContext.cs
@@ -10,11 +10,12 @@ namespace EFCorePoC.Models
     public class InvoiceDbContext : DbContext
     {
         public DbSet<Invoice> Invoices { get; set; }
+        public DbSet<Product> Products { get; set; }
 
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
-            //optionsBuilder.UseSqlServer("Data Source=SAGE019562;initial catalog=Invoice;persist security info=True;user id=user;password=user;");
-            optionsBuilder.UseSqlServer("Data Source=SAGE019153;initial catalog=Invoice;persist security info=True;user id=dan;password=dan;");
+            optionsBuilder.UseSqlServer("Data Source=SAGE019562;initial catalog=Invoice;persist security info=True;user id=user;password=user;");
+            //optionsBuilder.UseSqlServer("Data Source=SAGE019153;initial catalog=Invoice;persist security info=True;user id=dan;password=dan;");
         }
     }
 }
