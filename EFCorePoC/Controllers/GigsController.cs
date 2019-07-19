fd3d823c0a8de1baa1cc81c02d4e48d2a156e877
danraistrick <daniel.raistrick@sage.com>
2019-07-19 09:15:30 +0100

    genreal changes to the view

---
+++ EFCorePoC/Controllers/GigsController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using EFCorePoC.Models;
+using EFCorePoC.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EFCorePoC.Controllers
+{
+    public class GigsController : Controller
+    {
+        private InvoiceDbContext _context;
+
+        public GigsController()
+        {
+               _context = new InvoiceDbContext(); 
+        }
+        public IActionResult Create()
+        {
+            var viewModel = new GigFormViewModel
+            {
+                Genres = _context.Genres.ToList()
+
+            };
+            return View(viewModel);
+        }
+    }
+}
---
+++ EFCorePoC/Migrations/20190718133233_blankmigration.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+
+namespace EFCorePoC.Migrations
+{
+    public partial class blankmigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            
+            //SqlQuery("INSERT INTO Genres (Id, Name) VALUES (1, 'Jazz')");
+            //Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Blues')");
+            //Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Rock')");
+            //Sql("INSERT INTO Genres (Id, Name) VALUES (1, 'Country')");
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 1, "Jazz" });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 2, "Blues" });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 3, "Rock" });
+
+            migrationBuilder.InsertData(
+                table: "Genres",
+                columns: new[] { "Id", "Name" },
+                values: new object[] { 4, "Country" });
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
